8b6e087cd6cd6ff9f2f6e9f9e5443df8485b6eaa
Deeksha Sharma <71128747+Deeksha-Sharma-906@users.noreply.github.com>
2025-11-03 20:55:14 +0530

    Add files via upload

--- MailMerge.py
+++ MailMerge.py
@@ -11,5 +11,5 @@ with open("./Input/Letter/Starting_Letter.txt") as letter_file:
         stripped_name = name.strip()
         new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
         print(new_letter)
-        with open(f"./Output/Ready_To_Send_Files/Letter_For_{stripped_name}.docx", mode = "w") as completed_letter:
+        with open(f"./Output/Ready_To_Send_Files/Letter_For_{stripped_name}.txt", mode = "w") as completed_letter:
             completed_letter.write(new_letter)
